0e8db10181ec1edbfe18f3fc9cef36ba9da9f685
Eryckson Magno <eryckson@mbp-de-eryckson.home>
2017-02-19 16:45:14 -0300

    try-except media

--- yowsup/layers/axolotl/layer_receive.py
+++ yowsup/layers/axolotl/layer_receive.py
@@ -138,7 +138,12 @@ class AxolotlReceivelayer(AxolotlBaseLayer):
         whisperMessage = WhisperMessage(serialized=enc.getData())
         sessionCipher = self.getSessionCipher(encMessageProtocolEntity.getAuthor(False))
         plaintext = sessionCipher.decryptMsg(whisperMessage)
-        plaintext = plaintext.encode()
+        
+        try:
+            plaintext = plaintext.encode()
+        except AttributeError as ae:
+            pass
+        
         if enc.getVersion() == 2:
             paddingByte = plaintext[-1] if type(plaintext[-1]) is int else ord(plaintext[-1])
             padding = paddingByte & 0xFF
